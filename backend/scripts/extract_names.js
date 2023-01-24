1b2cafe6cbdeef332c843171e66ba910d1eb6b84
YuriiRG <yuriirg@tutanota.com>
2023-01-24 10:40:28 +0000

    Bugfix attempt

--- backend/scripts/extract_names.js
+++ backend/scripts/extract_names.js
@@ -3,4 +3,4 @@ const data = fs.readFileSync(process.argv[2], { encoding: 'utf-8' });
 const lines = data.trim().split('\n').filter(str => str.toLowerCase().includes('@amazon.com'));
 const parsed = lines.map(str => str.trim().split(','));
 const formatted = parsed.map((arr) => `${arr[2]} ${arr[1]}`).join('\n');
-fs.writeFileSync('names.txt', formatted);
+fs.writeFileSync('output_names.txt', formatted);
