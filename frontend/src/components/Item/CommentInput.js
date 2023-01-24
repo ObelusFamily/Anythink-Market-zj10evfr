bce73a1e6c42b8f76fa0898e39d47f4c118defe7
YuriiRG <yuriirg@tutanota.com>
2023-01-24 11:58:35 +0000

    Refactor CommentInput

--- frontend/src/components/Item/CommentInput.js
+++ frontend/src/components/Item/CommentInput.js
@@ -7,53 +7,41 @@ const mapDispatchToProps = (dispatch) => ({
   onSubmit: (payload) => dispatch({ type: ADD_COMMENT, payload }),
 });
 
-class CommentInput extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      body: "",
-    };
+function CommentInput({ slug, onSubmit, currentUser }) {
+  const [body, setBody] = useState("");
 
-    this.setBody = (ev) => {
-      this.setState({ body: ev.target.value });
-    };
+  createComment = async (e) => {
+    e.preventDefault();
+    const payload = await agent.Comments.create(slug, {
+      body
+    });
+    onSubmit(payload);
+    setBody("");
+  };
 
-    this.createComment = async (ev) => {
-      ev.preventDefault();
-      agent.Comments.create(this.props.slug, {
-        body: this.state.body,
-      }).then((payload) => {
-        this.props.onSubmit(payload);
-      });
-      this.setState({ body: "" });
-    };
-  }
-
-  render() {
-    return (
-      <form className="card comment-form m-2" onSubmit={this.createComment}>
-        <div className="card-block">
-          <textarea
-            className="form-control"
-            placeholder="Write a comment..."
-            value={this.state.body}
-            onChange={this.setBody}
-            rows="3"
-          ></textarea>
-        </div>
-        <div className="card-footer">
-          <img
-            src={this.props.currentUser.image}
-            className="user-pic mr-2"
-            alt={this.props.currentUser.username}
-          />
-          <button className="btn btn-sm btn-primary" type="submit">
-            Post Comment
-          </button>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <form className="card comment-form m-2" onSubmit={createComment}>
+      <div className="card-block">
+        <textarea
+          className="form-control"
+          placeholder="Write a comment..."
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+          rows="3"
+        ></textarea>
+      </div>
+      <div className="card-footer">
+        <img
+          src={currentUser.image}
+          className="user-pic mr-2"
+          alt={currentUser.username}
+        />
+        <button className="btn btn-sm btn-primary" type="submit">
+          Post Comment
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default connect(() => ({}), mapDispatchToProps)(CommentInput);
