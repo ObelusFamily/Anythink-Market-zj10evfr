6b5d9ecde3c761401bca449184809de7e882a0d9
YuriiRG <yuriirg@tutanota.com>
2023-01-24 13:06:09 +0000

    Fix imageless bug in API

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -28,7 +28,7 @@ const ItemPreview = (props) => {
       props.favorite(item.slug);
     }
   };
-  console.log(item.image);
+
   return (
     <div
       className="card bg-dark border-light p-3"
@@ -37,7 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image ? item.image : 'placeholder.png'}
+        src={item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
